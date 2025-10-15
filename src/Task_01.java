a1ca6f8d0032a5b4cea9abebc37d032eb311c9bb
616732 <miller.616732@swl.k12.oh.us>
2025-10-15 08:56:00 -0400

    10/15/25 8:55

---
+++ src/Task_01.java
@@ -0,0 +1,9 @@
+public class Task_01 {
+    public static void main(String[] args) {
+        int yourCount = 0;
+        for (int i = 1; i <= 30; i++) {
+            System.out.println(i);
+            yourCount = yourCount + 1;
+        }
+    }
+}

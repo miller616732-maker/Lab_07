b6e766c53eaca90cad04f5ad2438ed0fbf62eeae
616732 <miller.616732@swl.k12.oh.us>
2025-10-16 08:16:28 -0400

    10/16/25 8:16

--- src/Task_01.java
+++ src/Task_01.java
@@ -1,8 +1,24 @@
 public class Task_01 {
     public static void main(String[] args) {
         int yourCount = 0;
-        for (int i = 1; i <= 30; i++) {
-            System.out.println(i);
+        System.out.println("0 to 30");
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+            yourCount = yourCount + 1;
+        }
+        System.out.println("\n30 to 0");
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+            yourCount = yourCount - 1;
+        }
+        System.out.println("\n0 to 18 by 3");
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.print(i + " ");
+            yourCount = yourCount + 1;
+        }
+        System.out.println("\n10 to 0 by 2");
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.print(i + " ");
             yourCount = yourCount + 1;
         }
     }

be5ed3e3081ec938a891613fc1b6fe668831ba82
616732 <miller.616732@swl.k12.oh.us>
2025-10-17 08:15:56 -0400

    10/17/25 8:15

--- src/Task_03.java
+++ src/Task_03.java
@@ -3,31 +3,25 @@ public class Task_03 {
     public static void main(String[] args) {
         boolean valid = false;
         Random rand = new Random();
-        int rollOne;
-        int rollTwo;
-        int rollThree;
-        int rollSum;
+        int rollOne = 0;
+        int rollTwo = 0;
+        int rollThree = 0;
+        int rollSum = 0;
         int rollNumber = 0;
         System.out.printf("\n%s%6s%8s%10s%10s", "Roll Number", " Roll One", " Roll Two", " Roll Three", " Sum");
-        rollOne = rand.nextInt(6) + 1;
-        rollTwo = rand.nextInt(6) + 1;
-        rollThree = rand.nextInt(6) + 1;
-        rollSum = (rollOne + rollTwo + rollThree);
-        rollNumber++;
-        System.out.printf("\n%10d%9d%9d%9d%10d", rollNumber, rollOne, rollTwo, rollThree, rollSum);
         while (!valid) {
             rollOne = rand.nextInt(6) + 1;
             rollTwo = rand.nextInt(6) + 1;
             rollThree = rand.nextInt(6) + 1;
             rollSum = (rollOne + rollTwo + rollThree);
             rollNumber++;
-            System.out.printf("\n%10d%9d%9d%9d%10d", rollNumber, rollOne, rollTwo, rollThree, rollSum);
+            System.out.printf("\n%10d%9d%9d%9d%12d", rollNumber, rollOne, rollTwo, rollThree, rollSum);
             if (rollOne == rollTwo && rollOne == rollThree) {
                 valid = true;
             }
         }
         System.out.println("\nThe roll that worked was");
-        System.out.printf("\n%s%6s%8s%10s%10s", "Roll Number", " Roll One", " Roll Two", " Roll Three", " Sum");
-        System.out.printf("\n%10d%9d%9d%9d%10d", rollNumber, rollOne, rollTwo, rollThree, rollSum);
+        System.out.printf("\n%s%s%s%s%s", "Roll Number, ", " Roll One, ", " Roll Two, ", " Roll Three, ", " Sum");
+        System.out.printf("\n%10d%10d%10d%10d%10d", rollNumber, rollOne, rollTwo, rollThree, rollSum);
     }
 }
